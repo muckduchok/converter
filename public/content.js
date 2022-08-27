0f898f5ab46168178308013942c504b25ee69399
Anton <anton.avhutskiy@gmail.com>
2022-08-27 17:07:38 +0300

    add-currency-selection-in-modal

--- public/content.js
+++ public/content.js
@@ -5,23 +5,36 @@ let api_base = "https://api.exchangerate.host/latest?base=";
 function createHtml(uah, usd, pln) {
   return `<div id="converterPopup" style="position: fixed;top: 50%;left: 50%;min-width:120px;width: max-content;height: max-content;background: #282c34;padding: 10px;border-radius: 15px;display: flex;align-items: center;flex-direction: column;color: white;z-index: 999999">
         <div style="display: flex; justify-content: space-around; width: 100%">
+          <button style="color: darkseagreen;border: none;background: transparent;cursor: pointer;" class="cur-btn" value="UAH">&#10003;</button>
           <span>UAH:</span><span>${uah}</span>
         </div>
         <div style="display: flex; justify-content: space-around; width: 100%">
+          <button style="color: darkseagreen;border: none;background: transparent;cursor: pointer;" class="cur-btn" value="USD">&#10003;</button>
           <span>USD:</span><span>${usd}</span>
         </div>
         <div style="display: flex; justify-content: space-around; width: 100%">
+          <button style="color: darkseagreen;border: none;background: transparent;cursor: pointer;" class="cur-btn" value="PLN">&#10003;</button>
           <span>PLN:</span><span>${pln}</span>
         </div>
   </div>`
 }
 
-function showCalculateModal(rates) {
+async function getCurrencies(amount, currency) {
+    fetch(api_base + `${currency}&symbols=USD,UAH,PLN&amount=` + amount)
+        .then(r => r.text())
+        .then(result => {
+            let rates = JSON.parse(result)["rates"]
+            showCalculateModal(rates, amount);
+        })
+}
+
+function showCalculateModal(rates, amount) {
   let uah = rates["UAH"].toFixed(2)
   let usd = rates["USD"].toFixed(2);
   let pln = rates["PLN"].toFixed(2);
   if (!isNaN(uah)) {
     document.body.insertAdjacentHTML('beforeend',`${createHtml(uah, usd, pln)}`)
+    reSelectListener(amount);
   }
 }
 
@@ -31,20 +44,31 @@ document.addEventListener('mouseup', async () => {
 
   if (amount.length > 0 && !isDot) {
     chrome.storage.sync.get(['key'], function(cur) {
-      fetch(api_base + `${cur.key}&symbols=USD,UAH,PLN&amount=` + amount)
-          .then(r => r.text())
-          .then(result => {
-            let rates = JSON.parse(result)["rates"]
-            showCalculateModal(rates);
-          })
+        getCurrencies(amount, cur.key);
     });
   }
 })
 
 document.addEventListener('mousedown', async (e) => {
   document.querySelectorAll('#converterPopup').forEach((pop) => {
-    if (e.target.id !== 'converterPopup') {
+    if (e.target.id !== 'converterPopup' && e.target.localName !== 'button') {
       pop.remove()
     }
   });
 })
+
+function reSelectListener(amount) {
+    let plusButtons = document.querySelectorAll('.cur-btn');
+    plusButtons.forEach((btn) => {
+    btn.addEventListener('click', async (e) => {
+        let type = e.target.value;
+            setTimeout(() => {
+                getCurrencies(amount, type);
+                setTimeout(() => {
+                    document.querySelectorAll('.cur-btn')
+                        .forEach(btn => btn.remove());
+                }, 100)
+            }, 300)
+        })
+    })
+}
